7759d155c2399a01f486466ae92f2b741c82e463
Ofek Gila <ofek@theofekfoundation.org>
2016-09-09 15:23:55 -0700

    Cleanup some code

--- EightQueens.java
+++ EightQueens.java
@@ -7,73 +7,14 @@
  * September 8th, 2016 Update
  *
  * @author Ofek Gila
- * @version September 8th, 2016
+ * @version September 9th, 2016
  * @since September 8th, 2016
  */
 
 public class EightQueens	{
-	private int[][] threatenedBoard;
-	private int numSolutions;
-
-	EightQueens(int w)	{
-		threatenedBoard = new int[w][w];
-		for (int i = 0; i < threatenedBoard.length; i++)
-			for (int a = 0; a < threatenedBoard[i].length; a++)
-				threatenedBoard[i][a] = -1;
-		numSolutions = 0;
-	}
 	public static void main(String... pumpkins)	{
-		// EightQueens eq = new EightQueens(Integer.parseInt(pumpkins[0]));
-		// eq.run();
 		EightQueensRunner eqr = new EightQueensRunner(Integer.parseInt(pumpkins[0]), Integer.parseInt(pumpkins[1]), pumpkins.length > 2);
 		eqr.run();
-	}
-	public void run()	{
-		double startTime = System.nanoTime();
-		int i;
-		for (i = 0; i < (int)(threatenedBoard.length / 2); i++)
-			solveQueens(0, i, threatenedBoard);
-		int tempSolutions = numSolutions;
-		if (threatenedBoard.length % 2 == 1)
-			solveQueens(0, i, threatenedBoard);
-		numSolutions += tempSolutions;
-		System.out.printf("%,d solutions in %,f seconds\n", numSolutions, (System.nanoTime()-startTime) / 1E9);
-	}
-	private void solveQueens(int x, int y, int[][] threatenedBoard)	{
-		addQueen(x, y, threatenedBoard);
-		if (x == threatenedBoard.length - 1) {
-			numSolutions++;
-			threatenedBoard[x][y] = -1;
-		}	else {
-			for (int i = 0; i < threatenedBoard.length; i++)
-				if (threatenedBoard[x + 1][i] == -1)
-					solveQueens(x + 1, i, threatenedBoard);
-			removeQueen(x, y, threatenedBoard);
-		}
-	}
-	private void addQueen(int x, int y, int[][] threatenedBoard) {
-		int i, a;
-		for (i = x; i < threatenedBoard.length; i++)
-			if (threatenedBoard[i][y] == -1)
-				threatenedBoard[i][y] = x;
-		for (i = x + 1, a = y + 1; i < threatenedBoard.length && a < threatenedBoard[i].length; i++, a++)
-			if (threatenedBoard[i][a] == -1)
-				threatenedBoard[i][a] = x;
-		for (i = x + 1, a = y - 1; i < threatenedBoard.length && a >= 0; i++, a--)
-			if (threatenedBoard[i][a] == -1)
-				threatenedBoard[i][a] = x;
-	}
-	private void removeQueen(int x, int y, int[][] threatenedBoard) {
-		int i, a;
-		for (i = x; i < threatenedBoard.length; i++)
-			if (threatenedBoard[i][y] == x)
-				threatenedBoard[i][y] = -1;
-		for (i = x + 1, a = y + 1; i < threatenedBoard.length && a < threatenedBoard[i].length; i++, a++)
-			if (threatenedBoard[i][a] == x)
-				threatenedBoard[i][a] = -1;
-		for (i = x + 1, a = y - 1; i < threatenedBoard.length && a >= 0; i++, a--)
-			if (threatenedBoard[i][a] == x)
-				threatenedBoard[i][a] = -1;
 	}
 }
 
--- EightQueensUnoriginal.java
+++ EightQueensUnoriginal.java
@@ -1,76 +1,7 @@
-/*	Ofek Gila
-	November 18th, 2014
-	KnightTour.java
-	This solves the knight problem with an x by y sided threatenedBoard.
-*/
-/**
- * September 8th, 2016 Update
- *
- * @author Ofek Gila
- * @version September 8th, 2016
- * @since September 8th, 2016
- */
-
 public class EightQueensUnoriginal	{
-	private int[][] threatenedBoard;
-	private int numSolutions;
-
-	EightQueensUnoriginal(int w)	{
-		threatenedBoard = new int[w][w];
-		numSolutions = 0;
-	}
 	public static void main(String... pumpkins)	{
-		// EightQueensUnoriginal eq = new EightQueensUnoriginal(Integer.parseInt(pumpkins[0]));
-		// eq.run();
 		EightQueensUnoriginalRunner eqr = new EightQueensUnoriginalRunner(Integer.parseInt(pumpkins[0]), Integer.parseInt(pumpkins[1]), pumpkins.length > 2);
 		eqr.run();
-	}
-	public void run()	{
-		double startTime = System.nanoTime();
-		int i;
-		for (i = 0; i < (int)(threatenedBoard.length / 2); i++)
-			solveQueens(0, i, threatenedBoard);
-		int tempSolutions = numSolutions;
-		if (threatenedBoard.length % 2 == 1)
-			solveQueens(0, i, threatenedBoard);
-		numSolutions += tempSolutions;
-		System.out.printf("%,d solutions in %,f seconds\n", numSolutions, (System.nanoTime()-startTime) / 1E9);
-	}
-	private void solveQueens(int x, int y, int[][] threatenedBoard)	{
-		addQueen(x, y, threatenedBoard);
-		if (x == threatenedBoard.length - 1) {
-			numSolutions++;
-			threatenedBoard[x][y] = -1;
-		}	else {
-			for (int i = 0; i < threatenedBoard.length; i++)
-				if (threatenedBoard[x + 1][i] == -1)
-					solveQueens(x + 1, i, threatenedBoard);
-			removeQueen(x, y, threatenedBoard);
-		}
-	}
-	private void addQueen(int x, int y, int[][] threatenedBoard) {
-		int i, a;
-		for (i = x; i < threatenedBoard.length; i++)
-			if (threatenedBoard[i][y] == -1)
-				threatenedBoard[i][y] = x;
-		for (i = x + 1, a = y + 1; i < threatenedBoard.length && a < threatenedBoard[i].length; i++, a++)
-			if (threatenedBoard[i][a] == -1)
-				threatenedBoard[i][a] = x;
-		for (i = x + 1, a = y - 1; i < threatenedBoard.length && a >= 0; i++, a--)
-			if (threatenedBoard[i][a] == -1)
-				threatenedBoard[i][a] = x;
-	}
-	private void removeQueen(int x, int y, int[][] threatenedBoard) {
-		int i, a;
-		for (i = x; i < threatenedBoard.length; i++)
-			if (threatenedBoard[i][y] == x)
-				threatenedBoard[i][y] = -1;
-		for (i = x + 1, a = y + 1; i < threatenedBoard.length && a < threatenedBoard[i].length; i++, a++)
-			if (threatenedBoard[i][a] == x)
-				threatenedBoard[i][a] = -1;
-		for (i = x + 1, a = y - 1; i < threatenedBoard.length && a >= 0; i++, a--)
-			if (threatenedBoard[i][a] == x)
-				threatenedBoard[i][a] = -1;
 	}
 }
 
@@ -142,8 +73,6 @@ class EightQueensUnoriginalThread extends Thread {
 			solveQueens(queens, 1);
 		}
 		else {
-			// System.out.println(threadNum + " " + (int)(threadNum / 2) + " " + threadNum % 2);
-			// addQueen(0, (int)(threadNum / 2), threatenedBoard);
 			queens[0] = (int)(threadNum / 2);
 			if (threadNum % 2 == 0) {
 				for (int i = 0; i < (int)(queens.length / 2 + 0.6); i++) {
@@ -179,59 +108,6 @@ class EightQueensUnoriginalThread extends Thread {
 			if (queens[col] - queens[i] == col - i) return false;   // same minor diagonal
 		}
 		return true;
-	}
-
-	private void addQueen(int x, int y, int[][] threatenedBoard) {
-		int i, a;
-		for (i = x; i < threatenedBoard.length; i++)
-			if (threatenedBoard[i][y] == -1)
-				threatenedBoard[i][y] = x;
-		for (i = x + 1, a = y + 1; i < threatenedBoard.length && a < threatenedBoard[i].length; i++, a++)
-			if (threatenedBoard[i][a] == -1)
-				threatenedBoard[i][a] = x;
-		for (i = x + 1, a = y - 1; i < threatenedBoard.length && a >= 0; i++, a--)
-			if (threatenedBoard[i][a] == -1)
-				threatenedBoard[i][a] = x;
-		if (quites) {
-			if (x < threatenedBoard.length - 1) {
-				if (y > 1 && threatenedBoard[x+1][y-2] == -1)
-					threatenedBoard[x+1][y-2] = x;
-				if (y < threatenedBoard[x+1].length - 2 && threatenedBoard[x+1][y+2] == -1)
-					threatenedBoard[x+1][y+2] = x;
-			}
-			if (x < threatenedBoard.length - 2) {
-				if (y > 0 && threatenedBoard[x+2][y-1] == -1)
-					threatenedBoard[x+2][y-1] = x;
-				if (y < threatenedBoard[x+2].length - 1 && threatenedBoard[x+2][y+1] == -1)
-					threatenedBoard[x+2][y+1] = x;
-			}
-		}
-	}
-	private void removeQueen(int x, int y, int[][] threatenedBoard) {
-		int i, a;
-		for (i = x; i < threatenedBoard.length; i++)
-			if (threatenedBoard[i][y] == x)
-				threatenedBoard[i][y] = -1;
-		for (i = x + 1, a = y + 1; i < threatenedBoard.length && a < threatenedBoard[i].length; i++, a++)
-			if (threatenedBoard[i][a] == x)
-				threatenedBoard[i][a] = -1;
-		for (i = x + 1, a = y - 1; i < threatenedBoard.length && a >= 0; i++, a--)
-			if (threatenedBoard[i][a] == x)
-				threatenedBoard[i][a] = -1;
-		if (quites) {
-			if (x < threatenedBoard.length - 1) {
-				if (y > 1 && threatenedBoard[x+1][y-2] == x)
-					threatenedBoard[x+1][y-2] = -1;
-				if (y < threatenedBoard[x+1].length - 2 && threatenedBoard[x+1][y+2] == x)
-					threatenedBoard[x+1][y+2] = -1;
-			}
-			if (x < threatenedBoard.length - 2) {
-				if (y > 0 && threatenedBoard[x+2][y-1] == x)
-					threatenedBoard[x+2][y-1] = -1;
-				if (y < threatenedBoard[x+2].length - 1 && threatenedBoard[x+2][y+1] == x)
-					threatenedBoard[x+2][y+1] = -1;
-			}
-		}
 	}
 
 	public void start() {
